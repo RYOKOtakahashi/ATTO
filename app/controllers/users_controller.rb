c809ffb20f46fd18321f94efe388a788b9dba4ec
RYOKOtakahashi <ryoko.1109@gmail.com>
2018-09-25 21:20:31 +0900

    add: SNS link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name,
-																		items_attributes: [:item_name, :item_quantity, :price, :weight, :item_memo_one, :deadline, :unit, 
-																			:category, :subcategory, :item_image_id, :notice_day, :author, :maker, :color, :size, :use_place, 
-																			:purchase_place, :item_memo_two, :price_unit, :_destroy])
+				items_attributes: [:item_name, :item_quantity, :price, :weight, :item_memo_one, :deadline, :unit, 
+													:category, :subcategory, :item_image_id, :notice_day, :author, :maker, :color, :size, :use_place, 
+													:purchase_place, :item_memo_two, :price_unit, :_destroy])
 		end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 	def starred_by?(user)
     stars.where(user_id: user.id).exists?
   end
-  # favorited_by?メソッドで、引数で渡されたユーザのidがFavoritesテーブル内にexists?しているか調べる。
+  # starred_by?メソッド(別名_by?でもOK)で、引数で渡されたユーザのidがstarsテーブル内にexists?しているか調べる。
   # 存在していればTrue、存在していなければFalseを返すようにしている。
 
 end
